e8374837ccb718d27933bd3edfc140ef72ac70b0
Matias Sarapalo <sarapalo.matias@gmail.com>
2025-11-09 16:27:11 +0200

    week 2 task 4

--- viikko2/nhl-reader/src/statistics.py
+++ viikko2/nhl-reader/src/statistics.py
@@ -1,25 +1,45 @@
 import requests
 from player import Player
 
+class PlayerReader:
+
+    def __init__(self, url):
+        self.url = url
+    
+    def get_players(self):
+        url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+        response = requests.get(url).json()
+
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+
+class PlayerStats:
+
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        järjestetty = sorted(self.players, key=lambda p: p.goals + p.assists, reverse=True)
+
+        top_scorers = []
+
+        for player in järjestetty:
+            if player.nationality == nationality:
+                top_scorers.append(player)
+        return top_scorers
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    print("Suomalaiset pelaajat:")
-
-    järjestetty = sorted(players, key=lambda p: p.goals + p.assists, reverse=True)
-
-    for player in järjestetty:
-        if player.nationality == "FIN":
-            print(player)
+    for player in players:
+        print(player)
 
 main()
+
